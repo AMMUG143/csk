1f4e4479cc9ef7e179739daf578faf615e6e5774
AMMUG143 <samrutha958@gmail.com>
2025-04-12 06:15:08 -0800

    adding a new file

---
+++ add.py
@@ -0,0 +1,9 @@
+def add_numbers():
+    """Add two numbers and return the result."""
+    num1 = 5
+    num2 = 3
+    result = num1 + num2
+    return result
+
+if __name__ == "__main__":
+    print("The sum is:", add_numbers())
